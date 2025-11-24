d62b599ea282f93e3c276789e25cdf7118c08496
ojahongir2197 <ojahongir2197@gmail.com>
2025-11-24 15:35:56 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,51 @@
+soni = 0
+
+# 1-m
+a = "salom  dunyo  alik"
+print(f"text: {a}")
+
+for i in range(len(a)):
+    if a [i] == " ":
+        soni += 1
+
+print(f"probel soni: {soni} ")
+
+# 2-m
+a = "toshkentshahri"
+print(f"text: {a}")
+
+for i in range(len(a)):
+    if a[i] == " ":
+        soni += 1
+
+print(f" probel soni: {soni} ")
+
+# 3-m
+a = "  python  dasturlash  tili"
+print(f"text: {a}")
+
+for i in range(len(a)):
+    if a[i] == " ":
+        soni += 1
+
+print(f"probel soni : {soni}")
+
+# 4-m
+a = "salom dunyo!va alik?raxmat"
+print(f"text: {a}")
+
+for i in range(len(a)):
+    if a[i] == " ":
+        soni += 1
+
+print(f"probel soni: {soni}")
+
+# 5-m
+a = " a b c d e f g"
+print(f"text: {a}")
+
+for i in range(len(a)):
+    if a[i] == " ":
+        soni += 1
+
+print(f" probel soni: {soni}")
